887d624099bfa0379c9f45740ec3525bed095a75
newyork_loki <137760120+lowkeyrossi@users.noreply.github.com>
2025-08-16 11:09:00 +0530

    Update bundle_hdf5_whl.py

--- ci/bundle_hdf5_whl.py
+++ ci/bundle_hdf5_whl.py
@@ -21,7 +21,11 @@ def find_dlls():
     zlib_root = os.environ.get("ZLIB_ROOT")
     if zlib_root:
         print("ZLIB_ROOT", zlib_root)
-        yield os.path.join(zlib_root, 'bin_release', 'zlib.dll')
+        arch = platform.machine().lower()
+        if arch in ("arm64", "aarch64"):
+            yield os.path.join(zlib_root, 'bin', 'zlib.dll')
+        elif arch in ("amd64", "x86_64"):
+            yield os.path.join(zlib_root, 'bin_release', 'zlib.dll')
 
 def file_sha256(path):
     h = hashlib.sha256()
